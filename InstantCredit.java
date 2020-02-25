a650398de4373e4d1d4254bf7ca2a77d58e377e8
Sachin <subudhisachin73@gmail.com>
2020-02-25 23:35:05 +0530

    Adding first commit.

---
+++ InstantCredit.java
@@ -0,0 +1,8 @@
+  import static java.lang.Integer.MAX_VALUE;
+  import static java.lang.Byte.*;
+  public class InstantCredit {
+	//static int MAX_VALUE=655;	
+	public static void main(String[] args) {
+		System.out.println(MAX_VALUE);
+	}
+  }	
